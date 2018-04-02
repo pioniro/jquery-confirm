a787ce8b96ea5794251682d72ecd0eb724792d4d
Alexey <pioniro@yandex.ru>
2018-04-02 16:34:35 +0300

    Resolve my webpack issue

--- js/jquery-confirm.js
+++ js/jquery-confirm.js
@@ -7,39 +7,7 @@
  * Copyright 2013-2018 jquery-confirm
  * Licensed under MIT (https://github.com/craftpip/jquery-confirm/blob/master/LICENSE)
  */
-
-
-/**
- * UMD (Universal Module Definition) to support CommonJS, AMD and browser
- * Thanks to https://github.com/umdjs/umd
- */
-(function(factory){
-    if(typeof define === 'function' && define.amd){
-        // AMD. Register as an anonymous module.
-        define(['jquery', 'window'], factory);
-    }else if(typeof module === 'object' && module.exports){
-        // Node/CommonJS
-        module.exports = function(root, jQuery){
-            if(jQuery === undefined){
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if(typeof window !== 'undefined'){
-                    jQuery = require('jquery');
-                }
-                else{
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery, window);
-            return jQuery;
-        };
-    }else{
-        // Browser globals
-        factory(jQuery, window);
-    }
-}(function($, window){
+(function($, window){
     "use strict";
     var jconfirm, Jconfirm;
 
@@ -1369,4 +1337,4 @@
     $(document).on('mousedown', 'button, a', function(){
         jconfirm.lastClicked = $(this);
     });
-}));
+})(window.$, window);
